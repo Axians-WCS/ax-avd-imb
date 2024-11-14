5e4d29cfa7df8ee0987056979d92a8e4ef602e94
Luuk Ros <luuk.ros@axianswcs.com>
2024-11-14 15:48:03 +0100

    update ps

--- createManagedIdentityAib.ps1
+++ createManagedIdentityAib.ps1
@@ -3,12 +3,8 @@
     Creates a Managed Identity for Azure Image Builder and assigns the necessary permissions.
 
 .DESCRIPTION
-    This script automates the creation of a Managed Identity for Azure Image Builder, along with the required permissions.
-    It ensures that the Managed Identity can perform image creation tasks, access gallery images, and join virtual networks.
-    Additionally, it checks for and registers any missing resource providers needed for Azure Image Builder functionality.
-
-    The script downloads role definition templates, updates them with environment-specific values, and assigns custom roles 
-    to the Managed Identity in both the Image Builder resource group and the Virtual Network resource group.
+    This script creates a Managed Identity for Azure Image Builder, registers necessary resource providers,
+    and assigns custom roles for image creation and VNet access.
 
 .AUTHOR
     Michel Kleine & Luuk Ros
@@ -17,101 +13,68 @@
     14-11-2024
 
 .NOTES
-    - Ensure you have the appropriate permissions to manage Azure resources (Owner or User Access Administrator role).
-    - This script requires the Az PowerShell module.
-    - Customize the variables ($subscriptionID, $imageResourceGroup, $vnetResourceGroup, $location) to fit your environment.
+    - Requires Owner or User Access Administrator permissions.
+    - Az PowerShell module must be installed.
 
 .PREREQUISITES
-    - Install the Az PowerShell module:
-      Install-Module -Name Az -AllowClobber -Scope CurrentUser -Force
-    
-     - Have the URLs for the role definition templates (`aibRoleImageCreation.json` and `aibRoleVnetActions.json`) accessible.
-    
-      - Connect to your Azure account:
-      Connect-AzAccount
+    - Install-Module -Name Az -AllowClobber -Scope CurrentUser -Force
+    - Connect-AzAccount
 #>
 
 # Variables
-$subscriptionID = "setsubid" # Set the subscription ID where the resources will be deployed
-$imageResourceGroup = 'xyz-p1-imb-01' # Resource group name for the image builder resources
-$vnetResourceGroup = 'xyz-p1-hub-01' # Resource group where the vnet is deployed
+$subscriptionID = "setsubid"
+$imageResourceGroup = 'xyz-p1-imb-01'
+$vnetResourceGroup = 'xyz-p1-hub-01'
 $location = 'West Europe'
 
 # Set the subscription context
 Set-AzContext -SubscriptionId $subscriptionID
-Write-Output (Get-AzContext)
 
-# Register Features
+# Register required resource providers
 Get-AzResourceProvider -ProviderNamespace Microsoft.Compute, Microsoft.KeyVault, Microsoft.Storage, Microsoft.VirtualMachineImages, Microsoft.Network, Microsoft.ManagedIdentity, Microsoft.ContainerInstance |
   Where-Object RegistrationState -ne Registered |
     Register-AzResourceProvider
 
-# Create the resource group for image builder resources if it doesn't exist
+# Ensure the resource group exists
 New-AzResourceGroup -Name $imageResourceGroup -Location $location -ErrorAction SilentlyContinue
 
-# Generate Managed Identity name based on the resource group name
+# Generate a unique Managed Identity name
 $cleanedRgName = $imageResourceGroup -replace '-', ''
 $baseIdentityName = "$($cleanedRgName)mi"
 $index = 1
-
-# Loop to find the next available Managed Identity name
 do {
-    $identityName = "$baseIdentityName$($index.ToString("D2"))" # Format index with leading zeroes, e.g., mi01, mi02
+    $identityName = "$baseIdentityName$($index.ToString("D2"))"
     $existingIdentity = Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName -ErrorAction SilentlyContinue
     $index++
 } while ($existingIdentity -ne $null)
 
-Write-Output "Managed Identity Name: $identityName"
-
-# Create the User Identity
+# Create Managed Identity
 New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName -Location $location
-$identityNameResourceId = (Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).Id
 $identityNamePrincipalId = (Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).PrincipalId
 
-# set Role variables
+# Define roles
+$timeInt = $(Get-Date -UFormat '%s')
 $imageRoleDefName = "Azure Image Builder Image Def $timeInt"
 $networkRoleDefName = "Azure Image Builder VNet Role $timeInt"
 
-# Process aibRoleImageCreation.json
+# Download and configure aibRoleImageCreation.json
 $myRoleImageCreationUrl = 'https://raw.githubusercontent.com/Axians-WCS/ax-avd-imb/refs/heads/main/aibRoleImageCreation.json'
 $myRoleImageCreationPath = "myRoleImageCreation.json"
 Invoke-WebRequest -Uri $myRoleImageCreationUrl -OutFile $myRoleImageCreationPath -UseBasicParsing
-
-$Content = Get-Content -Path $myRoleImageCreationPath -Raw
-$Content = $Content -replace '<subscriptionID>', $subscriptionID
-$Content = $Content -replace '<rgName>', $imageResourceGroup
-$Content = $Content -replace 'Azure Image Builder Service Image Creation Role', $imageRoleDefName
-$Content | Out-File -FilePath $myRoleImageCreationPath -Force
-
+(Get-Content $myRoleImageCreationPath -Raw) -replace '<subscriptionID>', $subscriptionID -replace '<rgName>', $imageResourceGroup -replace 'Azure Image Builder Service Image Creation Role', $imageRoleDefName | Set-Content $myRoleImageCreationPath
 New-AzRoleDefinition -InputFile $myRoleImageCreationPath
 
-# Assign role to Managed Identity
-$RoleAssignParams = @{
-  ObjectId = $identityNamePrincipalId
-  RoleDefinitionName = $imageRoleDefName
-  Scope = "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
-}
-New-AzRoleAssignment @RoleAssignParams
+# Assign the image creation role
+New-AzRoleAssignment -ObjectId $identityNamePrincipalId -RoleDefinitionName $imageRoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
 
-# Process aibRoleVnetActions.json
+# Download and configure aibRoleVnetActions.json
 $myRoleVnetActionsUrl = 'https://raw.githubusercontent.com/Axians-WCS/ax-avd-imb/refs/heads/main/aibRoleVnetActions.json'
 $myRoleVnetActionsPath = "aibRoleVnetActions.json"
 Invoke-WebRequest -Uri $myRoleVnetActionsUrl -OutFile $myRoleVnetActionsPath -UseBasicParsing
-
-$VnetContent = Get-Content -Path $myRoleVnetActionsPath -Raw
-$VnetContent = $VnetContent -replace '<subscriptionID>', $subscriptionID
-$VnetContent = $VnetContent -replace '<vnetRgName>', $vnetResourceGroup
-$VnetContent = $VnetContent -replace 'Azure Image Builder Service Image Creation Role', $networkRoleDefName
-$VnetContent | Out-File -FilePath $myRoleVnetActionsPath -Force
-
+(Get-Content $myRoleVnetActionsPath -Raw) -replace '<subscriptionID>', $subscriptionID -replace '<vnetRgName>', $vnetResourceGroup -replace 'Azure Image Builder Service Image Creation Role', $networkRoleDefName | Set-Content $myRoleVnetActionsPath
 New-AzRoleDefinition -InputFile $myRoleVnetActionsPath
 
-# Assign VNet role to Managed Identity
-$RoleAssignVnetParams = @{
-  ObjectId = $identityNamePrincipalId
-  RoleDefinitionName = $networkRoleDefName
-  Scope = "/subscriptions/$subscriptionID/resourceGroups/$vnetResourceGroup"
-}
-New-AzRoleAssignment @RoleAssignVnetParams
+# Assign the VNet role
+New-AzRoleAssignment -ObjectId $identityNamePrincipalId -RoleDefinitionName $networkRoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$vnetResourceGroup"
 
 Write-Host "Managed Identity and role assignments created successfully."
